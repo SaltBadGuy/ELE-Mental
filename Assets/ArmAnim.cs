b25c1c86d717b5815f8c5f48f04ddd488539a296
SaltBadGuy <doomguytnt@gmail.com>
2019-06-09 16:15:57 +0100

    Embarassingly, looks like I've been working on the wrong unity project. Moved stuff over, need to remember I'm on Ele-mental Github now.

--- Assets/ArmAnim.cs
+++ Assets/ArmAnim.cs
@@ -13,6 +13,7 @@ public class ArmAnim : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Grabbing the Player State since that'll say whether character is hurt, happy, idle etc.
         PCS = this.gameObject.GetComponentInParent<PlayerState>();
 
     }
@@ -20,7 +21,7 @@ public class ArmAnim : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Grabs all the emotions from PlayerState and updates the animator variables.
+        //Grabs all the emotions from PlayerState and updates the animator variables. Means if we want the arms to do something when the character is idle, happy etc. we can do that.
         LAAnimCon.SetFloat("Boredom", PCS.Boredom);
         LAAnimCon.SetFloat("Anger", PCS.Anger);
         LAAnimCon.SetBool("Happy", PCS.Happy);
@@ -38,9 +39,27 @@ public class ArmAnim : MonoBehaviour
             LAAnimCon.SetBool("BlinkCDBool", false);
         }
 
+        //Ditto for right arm.
+        RAAnimCon.SetFloat("Boredom", PCS.Boredom);
+        RAAnimCon.SetFloat("Anger", PCS.Anger);
+        RAAnimCon.SetBool("Happy", PCS.Happy);
+        RAAnimCon.SetBool("Hurt", PCS.Hurt);
+
+        //Counts down the blink timer.
+        RAAnimCon.SetFloat("BlinkCDFloat", RAAnimCon.GetFloat("BlinkCDFloat") - 1);
+
+        if (RAAnimCon.GetFloat("BlinkCDFloat") > 0f)
+        {
+            RAAnimCon.SetFloat("BlinkCDFloat", RAAnimCon.GetFloat("BlinkCDFloat") - 1);
+        }
+        else
+        {
+            RAAnimCon.SetBool("BlinkCDBool", false);
+        }
+
     }
 
-    //Called when the blink animation ends, prevents the character from blinking for a random period of time
+    //Called when the blink animation ends, prevents the character from blinking for a random period of time (Right now this is just here for reference. Likely the arms will be involved in an animation we want to happen semi-randomly.)
     void BlinkEvent()
     {
         LAAnimCon.SetBool("BlinkCDBool", true);
--- Assets/Scripts/Player Character/PlayerState.cs
+++ Assets/Scripts/Player Character/PlayerState.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class PlayerState : MonoBehaviour
 {
-    public int Lives;
-    public float MaxHP;
-    public float CurHP;
 
     //Emotions, used for animation and facial changes
     public float Boredom;
     public float Anger;
     public bool Happy;
     public bool Hurt;
+    public int HurtCD;
 
+    //Misc states. ElementEquipped can be Fire, Water, Earth and Lightning.
     public bool Alive;
     public string ElementEquipped;
 
@@ -22,7 +21,6 @@ public class PlayerState : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        Lives = 3;
         ElementEquipped = "Fire";
         PS = GetComponent<PlayerShoot>();
     }
@@ -32,13 +30,23 @@ public class PlayerState : MonoBehaviour
     {
         Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
         Boredom++;
+        if(HurtCD > 0)
+        {
+            HurtCD--;
+        }
+        else
+        {
+            Hurt = false;
+        }
     }
 
     public void TakeDamage(float DamageTaken)//, Vector3 Impact)
     {
         //gameObject.GetComponent<Rigidbody2D>().AddRelativeForce(Impact * 5);
-        CurHP -= DamageTaken;
+        //CurHP -= DamageTaken;
         //DrawText(DamageTaken.ToString(), "Damage");
-        Debug.Log(gameObject.name + " took " + DamageTaken + " damage, leaving it at " + CurHP + "HP!");
+        //Debug.Log(gameObject.name + " took " + DamageTaken + " damage, leaving it at " + CurHP + "HP!");
+        Hurt = true;
+        HurtCD = 200;
     }
 }
